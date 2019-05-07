e778acdcfcae08685eee362218f6f31ff177a87e
Kabelo Martin Mayisa <kabelomayisa@outlook.com>
2019-05-07 11:58:41 +0200

    a feature where your list changes its format when the user hovers over it

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 //getting hold of the html elements to manipulate
 const addItemInput = document.querySelector('.addItemInput');//input field
+const listItems = document.getElementsByTagName('li');//getting hold of the list tags 
 
+/*
+-----INGORE FOR NOW --------
+//these list tags will change when the user hovers over them 
+for(let i = 0; i < listItems.length; i += 1){
+    listItems[i].addEventListener('mouseover', ()=>{
+        listItems[i].textContent = listItems[i].textContent.toUpperCase();
+        listItems[i].style.color = '#f305058c';
+        listItems[i].style.fontWeight = 'bold';
+        listItems[i].style.textDecoration = 'underline';
+    });
+//once mouse stops hovering on the list it will return to its normal format
+    listItems[i].addEventListener('mouseout', ()=>{
+        listItems[i].textContent = listItems[i].textContent.toLocaleLowerCase();
+        listItems[i].style.color = 'black';
+        listItems[i].style.fontWeight = 'none';
+        listItems[i].style.textDecoration = 'none';
+    })
+}
+*/
 
 //items typed into the input field will be added to a list **unordered list in index.html ***
 const addItem = () =>{
